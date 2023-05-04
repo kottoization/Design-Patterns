88ce25ab7935eca0ed5a48933bdc3408528a1562
kottoization <96475096+kottoization@users.noreply.github.com>
2023-05-04 17:06:57 +0200

    Add files via upload

---
+++ JuniorAccountType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount_StatePattern
+{
+    class JuniorAccountType : AccountType
+    {
+        protected const decimal withdrawLimit = 3000;
+
+        protected const decimal serviceFee = 0;
+
+        // Operacja wypłaty dla konta Junior
+        //
+        // Obiekt Account wywoła tę wersję metody Withdraw(),
+        // jeśli znajdzie się w "stanie" VIPAccountType.
+        public override void Withdraw(Account account, decimal amount)
+        {
+            if ((amount > withdrawLimit) || (account.Balance < amount + serviceFee))
+            {
+                Console.WriteLine("Withdrawal operation of {0} from Junior Account of {1} failed", amount, account.Owner);
+                return;
+            }
+
+            account.Balance -= amount + serviceFee;
+            Console.WriteLine("{0} was withdrowed from {1} Junior account", amount, account.Owner);
+        }
+
+        // Operacja zmiany rodzaju konta
+        //
+        // Obiekt Account wywoła tę wersję metody ChangeAccountType(),
+        // jeśli znajdzie się w "stanie" StandardAccountState.
+        public override void ChangeAccountType(Account account)
+        {
+            // jeśli saldo jest większe od 30000, konto Standard może być zmienione na VIP
+            
+                account.AccountType = new StandardAccountType();
+                Console.WriteLine("{0} Junior account was changed to Standard", account.Owner);            
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BankAccount_StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account johnAccount = new Account("John");
+            Account katieAccount = new Account("Katie");
+
+            johnAccount.Deposit(38000);
+            katieAccount.Deposit(4500);
+            Console.WriteLine("-----TEST-----");
+            Console.WriteLine("John's account type is " + johnAccount.AccountType.ToString());
+            Console.WriteLine("Katie's account type is " + katieAccount.AccountType.ToString());
+           
+            Console.WriteLine("----WITHDRAW-----");
+            Console.ReadKey();
+            johnAccount.Withdraw(3000);
+
+            Console.WriteLine("----WITHDRAW------");
+            Console.ReadKey();
+            johnAccount.Withdraw(20000);
+            katieAccount.Withdraw(20000);
+
+            Console.WriteLine("-----CHANGE-----");
+            Console.ReadKey();
+            johnAccount.ChangeAccountType();
+            katieAccount.ChangeAccountType();
+
+            Console.WriteLine("----WITHDRAW------");
+            Console.ReadKey();
+            johnAccount.Withdraw(10000);
+
+            Console.WriteLine("----CHANGE------");
+            Console.ReadKey();
+            johnAccount.ChangeAccountType();
+
+            Console.WriteLine("----WITHDRAW------");
+            Console.ReadKey();
+            johnAccount.Withdraw(10000);
+
+            Console.WriteLine("----------");
+            Console.ReadKey();
+        }
+    }
+}
