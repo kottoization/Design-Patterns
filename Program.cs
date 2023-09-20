c56d734b92533154ec5545bbce4e51548765e017
Mateusz Mulka <96475096+kottoization@users.noreply.github.com>
2023-09-20 17:39:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PerThreadSingleton
+{
+    internal class Program
+    {
+
+        public sealed class PerThreadSingleton
+        {
+            private static ThreadLocal<PerThreadSingleton> threadInstance
+                = new ThreadLocal<PerThreadSingleton>(
+                    () => new PerThreadSingleton()
+                    );
+            public int Id;
+            private PerThreadSingleton()
+            {
+                 Id = Thread.CurrentThread.ManagedThreadId;
+            }
+
+            public static PerThreadSingleton Instance => threadInstance.Value;
+        }
+        static void Main(string[] args)
+        {
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("t1: " + PerThreadSingleton.Instance.Id);
+            });
+
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+                Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+                Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
+            });
+            Task.WaitAll(t1, t1);
+        }
+    }
+}
