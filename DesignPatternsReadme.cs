0877d317fda1143e50e09d68dfda7dfbe5924551
Mateusz Mulka <96475096+kottoization@users.noreply.github.com>
2023-12-05 17:44:46 +0100

    Update DesignPatternsReadme.cs

--- DesignPatternsReadme.cs
+++ DesignPatternsReadme.cs
@@ -1,4 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, this is a project designed to take you around my Design Patterns project!");
 Console.WriteLine("As explained in Readme, the main goal of this project is to show basic understandment of some of the most popular patterns.");
 Console.WriteLine("I might be adding some new content to this repository in the future.");
